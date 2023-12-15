f057b0dfebe168ef5988e3b757332b08c79aa0e0
Elisson Guímel da Silva <guigolawliet13@gmail.com>
2023-12-15 13:18:34 -0300

    feat: Cat lisy

---
+++ src/components/Cafe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cat from './Cat';
+
+const Cafe = () => {
+    return (
+        <>
+            <Cat name="Maru" />
+            <Cat name="Jellylorum" />
+            <Cat name="Spot" />
+        </>
+    );
+};
+
+export default Cafe;
